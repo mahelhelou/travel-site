b9144f8bea4e1e0735352ac9e73d2f0d8b0da39d
Mahmoud El Helou <mahelhelou@gmail.com>
2018-12-06 07:46:35 +0200

    Completed Footer section

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite');
+
+var config = {
+    mode: {
+        css: {
+
+        }
+    }
+}
+
+gulp.task('createSprite', function() {
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite'));
+})
