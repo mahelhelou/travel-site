32be9a8107ecdc60d69a62103edb6faef3d9f073
Mahmoud El Helou <mahelhelou@gmail.com>
2019-06-22 12:40:29 +0300

    Main Menu is now complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,21 +4,8 @@
                 ### jqeury.js
 
             ## Modules Files
-                ### Person.js
+                ### MobileMenu
 ==============================*/
+import MobileMenu from './modules/MobileMenu'
 
-const $ = require('jquery')
-import Person from './modules/Person'
-
-class Adult extends Person {
-    payTaxes() {
-        console.log('Now ' + this.name + ' owes $0 in taxes')
-    }
-}
-
-let john = new Person('John Doe', 'red')
-john.greet()
-
-let jane = new Adult('Jane Smith', 'orange')
-jane.greet()
-jane.payTaxes()
+const moibleMenu = new MobileMenu
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -10,3 +10,19 @@ class Person {
 }
 
 export default Person
+
+// Writting in App.js
+/* import Person from './modules/Person'
+
+class Adult extends Person {
+    payTaxes() {
+        console.log('Now ' + this.name + ' owes $0 in taxes')
+    }
+}
+
+let john = new Person('John Doe', 'red')
+john.greet()
+
+let jane = new Adult('Jane Smith', 'orange')
+jane.greet()
+jane.payTaxes() */
